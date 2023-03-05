4c64bb19ceb92715475beb8e1824ecb091385137
Xander Gonzaga <xandergonzaga@hotmail.com>
2023-03-06 10:04:41 +1100

    Bundles

---
+++ bundles/ucc-main/extension/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function (nodecg) {
+	
+	// Production data to be access by graphics and external bundles
+	// Team Names
+	// Team Scores
+	// Game Text (Info Box)
+	// Main Title (UNSW Contenders Cup 2xTx)
+	// Secondary Title (Valorant Premier)
+};
