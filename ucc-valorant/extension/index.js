bddf2cda9c01b59f1e4cf66b8d185e4d3b711005
Xander Gonzaga <xandergonzaga@hotmail.com>
2024-09-21 03:18:03 +1000

    new scoreboard

--- ucc-valorant/extension/index.js
+++ ucc-valorant/extension/index.js
@@ -1,13 +1,174 @@
 'use strict';
 
 module.exports = function (nodecg) {
-	nodecg.log.info('Hello, from your bundle\'s extension!');
-	nodecg.log.info('I\'m where you put all your server-side code.');
-	nodecg.log.info(`To edit me, open "${__filename}" in your favorite text editor or IDE.`);
-	nodecg.log.info('You can use any libraries, frameworks, and tools you want. There are no limits.');
-	nodecg.log.info('Visit https://nodecg.com for full documentation.');
-	nodecg.log.info('Good luck!');
+	const router = nodecg.Router();
+	const team_scoreboard = {
+		"team_0": [],
+		"team_1": []
+	}
 
+	const AGENT_MAP = {
+		"Clay"			: "Raze",
+		"Pandemic"	: "Viper",
+		"Wraith"		: "Omen",
+		"Hunter"		: "Sova",
+		"Thorne"		: "Sage",
+		"Phoenix"		: "Phoenix",
+		"Wushu"			: "Jett",
+		"Gumshoe"		: "Cypher",
+		"Sarge"			: "Brimstone",
+		"Breach"		: "Breach",
+		"Vampire"		: "Reyna",
+		"Killjoy"		: "Killjoy",
+		"Guide"			: "Skye",
+		"Stealth"		: "Yoru",
+		"Rift"			: "Astra",
+		"Grenadier"	: "KAYO",
+		"Deadeye"		: "Chamber",
+		"Sprinter"	: "Neon",
+		"BountyHunter": "Fade",
+		"Mage"			: "Harbor",
+		"Aggrobot"	: "Gekko",
+		"Cable"			: "Deadlock",
+		"Sequoia"		: "Iso",
+		"Smonk"			: "Clove",
+		"Nox"				: "Vyse"
+	}
+
+	const WEAPON_MAP = {
+		"TX_Hud_Pistol_Classic"				: "Classic",
+		"TX_Hud_Pistol_Slim"					: "Shorty",
+		"TX_Hud_Pistol_AutoPistol"		: "Frenzy",
+		"TX_Hud_Pistol_Luger"					: "Ghost",
+		"TX_Hud_Pistol_Sheriff"				: "Sheriff",
+		"TX_Hud_Shotguns_Pump"				: "Bucky",
+		"TX_Hud_Shotguns_Persuader"		: "Judge",
+		"TX_Hud_SMGs_Vector"					: "Stinger",
+		"TX_Hud_SMGs_Ninja"						: "Spectre",
+		"TX_Hud_Rifles_Burst"					: "Bulldog",
+		"TX_Hud_Rifles_DMR"						: "Guardian",
+		"TX_Hud_Rifles_Ghost"					: "Phantom",
+		"TX_Hud_Rifles_Volcano"				: "Vandal",
+		"TX_Hud_Sniper_Bolt"					: "Marshal",
+		"TX_Hud_Sniper_Operater"			: "Operator",
+		"TX_Hud_Sniper_DoubleSniper"	: "Outlaw",
+		"TX_Hud_LMG"									:	"Ares",
+		"TX_Hud_HMG"									:	"Odin",
+		"knife"												: "Knife"
+	};
+
+	const rpc_ucc_valtest = nodecg.Replicant('rpc_ucc_valtest');
+	const rpc_gsi_roundNo = nodecg.Replicant('val_roundNo', { 
+		defaultValue: -1
+	});
+	const rpc_gsi_roundPhase = nodecg.Replicant('val_roundPhase', {
+		defaultValue: "unknown"
+	});
+	const rpc_gsi_scoreboard = nodecg.Replicant('val_scoreboard', {
+		defaultValue: team_scoreboard
+	});
+	const rpc_gsi_creditDeltas = nodecg.Replicant('val_creditDeltas', {
+		defaultValue: {
+			"scoreboard_0": {"credits": 0, "delta": 0},
+			"scoreboard_1": {"credits": 0, "delta": 0},
+			"scoreboard_2": {"credits": 0, "delta": 0},
+			"scoreboard_3": {"credits": 0, "delta": 0},
+			"scoreboard_4": {"credits": 0, "delta": 0},
+			"scoreboard_5": {"credits": 0, "delta": 0},
+			"scoreboard_6": {"credits": 0, "delta": 0},
+			"scoreboard_7": {"credits": 0, "delta": 0},
+			"scoreboard_8": {"credits": 0, "delta": 0},
+			"scoreboard_9": {"credits": 0, "delta": 0}
+		}
+	});
+	let lockMoney = false;
+
+
+	// v1 - Initial implementation using REST API
+	// Move to WebSocket later for better continuous updates
+	router.post('/api/gameStats', (req, res) => {
+		const gameStatsJSON = req.body;
+		rpc_ucc_valtest.value = req.body;
+
+		if (gameStatsJSON.hasOwnProperty("info") 
+		 && gameStatsJSON["feature"] == "match_info") 
+		{
+			const gameMatchInfo = gameStatsJSON["info"]["match_info"];
+			const scoreboardArray = Object.keys(gameMatchInfo).filter(k => k.startsWith("scoreboard_"));
+			const rosterArray = Object.keys(gameMatchInfo).filter(k => k.startsWith("roster_"));
+
+			if (gameMatchInfo.hasOwnProperty("round_number")) {
+				rpc_gsi_roundNo.value = gameMatchInfo["round_number"];
+			} else if (gameMatchInfo.hasOwnProperty("round_phase")) {
+				rpc_gsi_roundPhase.value = gameMatchInfo["round_phase"];
+
+				// Handle locking start of round credits count
+				if (gameMatchInfo["round_phase"] == "end") {
+					lockMoney = false;
+				} else if (gameMatchInfo["round_phase"] == "shopping") {
+					setTimeout(() => {
+						lockMoney = true;
+					}, 500);
+				}
+			} else if (scoreboardArray !== undefined && scoreboardArray.length > 0) { 
+				for (let scoreboardObj of scoreboardArray) {
+					const playerScoreboard = JSON.parse(gameMatchInfo[scoreboardObj]);
+					// Update player data
+					playerScoreboard["character"] = AGENT_MAP[playerScoreboard["character"]];
+					playerScoreboard["name"] = playerScoreboard["name"].replace(/#.{3,5}$/g, "");
+					playerScoreboard["weapon"] = WEAPON_MAP[playerScoreboard["weapon"]];
+					playerScoreboard["ultPoints"] = playerScoreboard["ult_points"];
+					playerScoreboard["ultMax"] = playerScoreboard["ult_max"];
+					playerScoreboard["rawIndex"] = scoreboardObj;
+					
+					const playerTeam = team_scoreboard[`team_${playerScoreboard["team"]}`];
+					
+					// Start of Round Credits and Delta Handling
+					if (rpc_gsi_roundPhase.value == "shopping") {
+						const currentMap = rpc_gsi_creditDeltas.value;
+
+						if (currentMap[scoreboardObj] === undefined) {
+							currentMap[scoreboardObj] = {"credits": -1, "delta": 0};
+						}
+
+						if (!lockMoney) {
+							currentMap[scoreboardObj]["credits"] = playerScoreboard["money"];
+						}
+
+						currentMap[scoreboardObj]["delta"] = 
+							parseInt(playerScoreboard["money"]) 
+								- parseInt(currentMap[scoreboardObj]["credits"]);
+
+						rpc_gsi_creditDeltas.value = currentMap;
+
+						// Update scoreboard replicant
+						let isExist = false;
+						for (let playerI in playerTeam) {
+							if (playerTeam[playerI]["rawIndex"] === scoreboardObj) {
+								playerTeam[playerI] = playerScoreboard;
+								isExist = true;
+							}
+						}
+
+						if (!isExist) {
+							playerTeam.push(playerScoreboard);
+						}
+					}
+				}
+
+				rpc_gsi_scoreboard.value = team_scoreboard;
+				console.log(JSON.stringify(team_scoreboard));
+			} else if (rosterArray !== undefined && rosterArray.length > 0) {
+				// Update roster data
+				for (let roster of rosterArray) {
+					console.log(gameMatchInfo[roster]);
+				}
+			}
+		}
+
+		res.sendStatus(200);
+	});
+	
 	nodecg.sendMessageToBundle(
 		'registerOverlay', 
 		'ucc-main', 
@@ -16,4 +177,14 @@ module.exports = function (nodecg) {
 			`${nodecg.bundleName}/graphics/index.html`
 		]
 	);
+
+	router.options('/api/gameStats', (req, res) => {
+		res.status(200)
+		   .header('Access-Control-Allow-Origin', '*')
+		   .header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+		   .header('Access-Control-Allow-Headers', 'Content-Type')
+		   .send();
+	});
+
+	nodecg.mount('/ucc-valorant', router);
 };
---
+++ ucc-valorant/graphics/scoreboard/script.js
@@ -0,0 +1,91 @@
+/* Element Constants */
+const left_scoreboard   = document.getElementById('left_scoreboard');
+const right_scoreboard  = document.getElementById('right_scoreboard');
+
+/* NodeCG Replicants */
+const rpc_gsi_scoreboard = nodecg.Replicant('val_scoreboard');
+const rpc_gsi_creditDeltas = nodecg.Replicant('val_creditDeltas');
+const rpc_gsi_overlay_state = nodecg.Replicant('rpc_gsi_overlay_state', 'ucc-valorant', { defaultValue: false });
+
+/* NodeCG Event Listeners */
+rpc_gsi_overlay_state.on('change', (newValue, oldValue) => {
+  left_scoreboard.classList.toggle('visible', newValue);
+  right_scoreboard.classList.toggle('visible', newValue);
+});
+
+rpc_gsi_scoreboard.on('change', (newValue, oldValue) => {
+  for (let i = 0; i < Object.keys(newValue).length; i++) {
+    updateSide(newValue, 0);
+    updateSide(newValue, 1);
+    // updateIndividual(newValue, Object.keys(newValue)[i]);
+  }
+});
+
+rpc_gsi_creditDeltas.on('change', (newValue, oldValue) => {
+  for (const player in newValue) {
+    const creditObj = newValue[player];
+
+    let playerGroup = undefined;
+    const players = document.getElementsByClassName("player");
+    for (let e of players) {
+      console.log(e);
+      if (e.dataset.rawIndex == player) {
+        playerGroup = e;
+        break;
+      }
+    }
+    if (playerGroup === undefined) continue;
+    playerGroup.getElementsByClassName("delta")[0].style.display = 
+      parseInt(creditObj["delta"]) < 0 ? "block" : "none";
+    playerGroup.getElementsByClassName("delta")[0].textContent = creditObj["delta"];
+    playerGroup.getElementsByClassName("current")[0].textContent = creditObj["credits"];
+  }
+
+  console.log(JSON.stringify(newValue));
+});
+
+/* NodeCG Updates */
+
+/* Function */
+const updateSide = (data, team) => {
+  const teamId = `team_${team}`;
+  for (let player in data[teamId]) {
+    updateIndividual(data, teamId, player); 
+  }
+}
+
+const updateIndividual = (data, team, playerIndex) => {
+  console.log(team);
+  const scoreboardObj = data[team][playerIndex];
+  console.log(scoreboardObj);
+  const scoreboard_side = team === 'team_1' ? left_scoreboard : right_scoreboard;
+  const playerGroup = scoreboard_side.getElementsByClassName("player")[playerIndex];
+  playerGroup.dataset.rawIndex = scoreboardObj["rawIndex"];
+  playerGroup.getElementsByClassName("agentIcon")[0].src = `./graphics/agents/${scoreboardObj["character"]}.webp`;
+  playerGroup.getElementsByClassName("name")[0].textContent = scoreboardObj["name"];
+  playerGroup.getElementsByClassName("kda")[0].textContent = 
+    `${scoreboardObj["kills"]}/${scoreboardObj["deaths"]}/${scoreboardObj["assists"]}`;
+  playerGroup.getElementsByClassName("loadout")[0].children[0].src = 
+    `./graphics/loadout/${scoreboardObj["weapon"]}.webp`;
+  playerGroup.getElementsByClassName("current")[0].textContent = scoreboardObj["money"];
+
+  const ultDiamonds = playerGroup.getElementsByClassName("ultimate")[0].children;
+  const ultPoints = parseInt(scoreboardObj["ult_points"]);
+  const ultMax = parseInt(scoreboardObj["ult_max"]);
+  for (let i in ultDiamonds) {
+    if (i == 'length') break;
+
+    if (parseInt(i) >= ultMax) {
+      console.log(i, ultMax);
+      playerGroup.getElementsByClassName("ultimate")[0].children[i].style.display = "none";
+    } else {
+      playerGroup.getElementsByClassName("ultimate")[0].children[i].style.display = "block";
+    }
+
+    if (parseInt(i) >= ultPoints) {
+      ultDiamonds[i].style.color = "rgb(100, 100, 100)";
+    } else {
+      ultDiamonds[i].style.color = "white";
+    }
+  }
+}
