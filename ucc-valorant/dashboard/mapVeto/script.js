b6e0dc86463006c20ea31c4c629a6697cfd174e9
Xander Gonzaga <x.gonzaga@student.unsw.edu.au>
2025-04-12 04:15:02 +1000

    feat: best of three for mapveto

--- ucc-valorant/dashboard/mapVeto/script.js
+++ ucc-valorant/dashboard/mapVeto/script.js
@@ -2,21 +2,29 @@
 const btnUpdate         = document.getElementById('updateButton');
 const switchVisibility  = document.getElementById('toggleVisibility');
 
-const map1Selection     = document.getElementById('map1Select');
-const map2Selection     = document.getElementById('map2Select');
-const map3Selection     = document.getElementById('map3Select');
-const map4Selection     = document.getElementById('map4Select');
-const map5Selection     = document.getElementById('map5Select');
-const map6Selection     = document.getElementById('map6Select');
-const map7Selection     = document.getElementById('map7Select');
+const map1Div           = document.getElementById('map1');
+const map2Div           = document.getElementById('map2');
+const map3Div           = document.getElementById('map3');
+const map4Div           = document.getElementById('map4');
+const map5Div           = document.getElementById('map5');
+const map6Div           = document.getElementById('map6');
+const map7Div           = document.getElementById('map7');
 
-const map1Text          = document.getElementById('map1Text');
-const map2Text          = document.getElementById('map2Text');
-const map3Text          = document.getElementById('map3Text');
-const map4Text          = document.getElementById('map4Text');
-const map5Text          = document.getElementById('map5Text');
-const map6Text          = document.getElementById('map6Text');
-const map7Text          = document.getElementById('map7Text');
+const map1Selection     = map1Div.getElementsByClassName('mapSelect')[0];
+const map2Selection     = map2Div.getElementsByClassName('mapSelect')[0];
+const map3Selection     = map3Div.getElementsByClassName('mapSelect')[0];
+const map4Selection     = map4Div.getElementsByClassName('mapSelect')[0];
+const map5Selection     = map5Div.getElementsByClassName('mapSelect')[0];
+const map6Selection     = map6Div.getElementsByClassName('mapSelect')[0];
+const map7Selection     = map7Div.getElementsByClassName('mapSelect')[0];
+
+const map1Text          = map1Div.getElementsByClassName('mapSubtext')[0];
+const map2Text          = map2Div.getElementsByClassName('mapSubtext')[0];
+const map3Text          = map3Div.getElementsByClassName('mapSubtext')[0];
+const map4Text          = map4Div.getElementsByClassName('mapSubtext')[0];
+const map5Text          = map5Div.getElementsByClassName('mapSubtext')[0];
+const map6Text          = map6Div.getElementsByClassName('mapSubtext')[0];
+const map7Text          = map7Div.getElementsByClassName('mapSubtext')[0];
 
 const btnInflate        = document.getElementById('inflation');
 const btnDeflate        = document.getElementById('deflation');
@@ -34,10 +42,43 @@ const replMapBan5 = nodecg.Replicant('mapBan5');
 const replMapBan6 = nodecg.Replicant('mapBan6');
 const replMapBan7 = nodecg.Replicant('mapBan7');
 
+/* Mapping */
+
+const MAP_MAPPING = {
+  1: {
+    "replicant": replMapBan1,
+    "div": map1Div,
+  },
+  2: {
+    "replicant": replMapBan2,
+    "div": map2Div,
+  },
+  3: {
+    "replicant": replMapBan3,
+    "div": map3Div,
+  },
+  4: {
+    "replicant": replMapBan4,
+    "div": map4Div,
+  },
+  5: {
+    "replicant": replMapBan5,
+    "div": map5Div,
+  },
+  6: {
+    "replicant": replMapBan6,
+    "div": map6Div,
+  },
+  7: {
+    "replicant": replMapBan7,
+    "div": map7Div,
+  }
+}
+
 /* NodeCG Event Listeners */
-replMapsToggle.on('change', (newValue, oldValue) => {
-  switchVisibility.checked = newValue;
-});
+// replMapsToggle.on('change', (newValue, oldValue) => {
+//   switchVisibility.checked = newValue;
+// });
 
 replMapImagesAsset.on('change', (newValue, oldValue) => {
   map1Selection.innerHTML = '';
@@ -72,11 +113,13 @@ replMapBan2.on('change', (newValue, oldValue) => {
 replMapBan3.on('change', (newValue, oldValue) => {
   map3Selection.value = newValue?.id;
   map3Text.value = newValue?.text;
+  map3Div.getElementsByClassName('mapActiveToggle')[0].checked = newValue?.active;
 });
 
 replMapBan4.on('change', (newValue, oldValue) => {
   map4Selection.value = newValue?.id;
   map4Text.value = newValue?.text;
+  map4Div.getElementsByClassName('mapActiveToggle')[0].checked = newValue?.active;
 });
 
 replMapBan5.on('change', (newValue, oldValue) => {
@@ -92,6 +135,7 @@ replMapBan6.on('change', (newValue, oldValue) => {
 replMapBan7.on('change', (newValue, oldValue) => {
   map7Selection.value = newValue?.id;
   map7Text.value = newValue?.text;
+  map7Div.getElementsByClassName('mapActiveToggle')[0].checked = newValue?.active;
 });
 
 /* NodeCG Updates */
@@ -114,7 +158,8 @@ btnUpdate.onclick = () => {
     "imageUrl": replMapImagesAsset.value[map1Selection.value]["url"],
     "videoUrl": replMapBan1Video,
     "id": map1Selection.value,
-    "text": map1Text.value
+    "text": map1Text.value,
+    "active": map1Div.getElementsByClassName('mapActiveToggle')[0]?.checked ?? false
   };
 
   let replMapBan2Video = replMapVideosAsset.value.filter(
@@ -125,7 +170,8 @@ btnUpdate.onclick = () => {
     "imageUrl": replMapImagesAsset.value[map2Selection.value]["url"],
     "videoUrl": replMapBan2Video,
     "id": map2Selection.value,
-    "text": map2Text.value
+    "text": map2Text.value,
+    "active": map2Div.getElementsByClassName('mapActiveToggle')[0]?.checked ?? false
   };
 
   let replMapBan3Video = replMapVideosAsset.value.filter(
@@ -136,7 +182,8 @@ btnUpdate.onclick = () => {
     "imageUrl": replMapImagesAsset.value[map3Selection.value]["url"],
     "videoUrl": replMapBan3Video,
     "id": map3Selection.value,
-    "text": map3Text.value
+    "text": map3Text.value,
+    "active": map3Div.getElementsByClassName('mapActiveToggle')[0]?.checked ?? false
   };
 
   let replMapBan4Video = replMapVideosAsset.value.filter(
@@ -147,7 +194,8 @@ btnUpdate.onclick = () => {
     "imageUrl": replMapImagesAsset.value[map4Selection.value]["url"],
     "videoUrl": replMapBan4Video,
     "id": map4Selection.value,
-    "text": map4Text.value
+    "text": map4Text.value,
+    "active": map4Div.getElementsByClassName('mapActiveToggle')[0]?.checked ?? false
   };
 
   let replMapBan5Video = replMapVideosAsset.value.filter(
@@ -158,7 +206,8 @@ btnUpdate.onclick = () => {
     "imageUrl": replMapImagesAsset.value[map5Selection.value]["url"],
     "videoUrl": replMapBan5Video,
     "id": map5Selection.value,
-    "text": map5Text.value
+    "text": map5Text.value,
+    "active": map5Div.getElementsByClassName('mapActiveToggle')[0]?.checked ?? false
   };
 
   let replMapBan6Video = replMapVideosAsset.value.filter(
@@ -169,7 +218,8 @@ btnUpdate.onclick = () => {
     "imageUrl": replMapImagesAsset.value[map6Selection.value]["url"],
     "videoUrl": replMapBan6Video,
     "id": map6Selection.value,
-    "text": map6Text.value
+    "text": map6Text.value,
+    "active": map6Div.getElementsByClassName('mapActiveToggle')[0]?.checked ?? false
   };
 
   let replMapBan7Video = replMapVideosAsset.value.filter(
@@ -180,11 +230,24 @@ btnUpdate.onclick = () => {
     "imageUrl": replMapImagesAsset.value[map7Selection.value]["url"],
     "videoUrl": replMapBan7Video,
     "id": map7Selection.value,
-    "text": map7Text.value
+    "text": map7Text.value,
+    "active": map7Div.getElementsByClassName('mapActiveToggle')[0]?.checked ?? false
   };
 };
 
 /* Functions */
 function checkboxHandling(chkbox){
   replMapsToggle.value = switchVisibility.checked;
+}
+
+const handleActiveMap = (mapNo) => {
+  for (let [index, element] of Object.entries(MAP_MAPPING)){
+    if (index != mapNo){
+      const toggleElement = element.div.getElementsByClassName('mapActiveToggle')[0];
+      console.log(index, mapNo, toggleElement);
+      if (!toggleElement) continue;
+
+      toggleElement.checked = false;
+    }
+  }
 }
---
+++ ucc-valorant/graphics/mapVeto/bo3Fullscreen/script.js
@@ -0,0 +1,181 @@
+/* Element Constants */
+const eMapBan1Div = document.getElementById('mapBan1');
+const eMapBan2Div = document.getElementById('mapBan2');
+const eMapBan3Div = document.getElementById('mapBan3');
+const eMapBan4Div = document.getElementById('mapBan4');
+const eMapBan5Div = document.getElementById('mapBan5');
+const eMapBan6Div = document.getElementById('mapBan6');
+const eMapBan7Div = document.getElementById('mapBan7');
+const expansionCompensation = document.getElementById('expansionCompensation');
+const eAudioBuild1 = document.getElementById('build1');
+const eAudioBuild2 = document.getElementById('build2');
+
+/* Variable */
+let activeMap = eMapBan7Div;
+
+/* NodeCG Replicants */
+const replAudioBuild1 = nodecg.Replicant('assets:mapvetobuild1');
+const replAudioBuild2 = nodecg.Replicant('assets:mapvetobuild2');
+const replMapBan1 = nodecg.Replicant('mapBan1');
+const replMapBan2 = nodecg.Replicant('mapBan2');
+const replMapBan3 = nodecg.Replicant('mapBan3');
+const replMapBan4 = nodecg.Replicant('mapBan4');
+const replMapBan5 = nodecg.Replicant('mapBan5');
+const replMapBan6 = nodecg.Replicant('mapBan6');
+const replMapBan7 = nodecg.Replicant('mapBan7');
+
+/* NodeCG Event Listeners */
+replAudioBuild1.on('change', (newValue, oldValue) => {
+  eAudioBuild1.src = newValue[0]?.["url"];
+  eAudioBuild1.load();
+  eAudioBuild1.play();
+});
+
+replAudioBuild2.on('change', (newValue, oldValue) => {
+  eAudioBuild2.src = newValue[0]?.["url"];
+  eAudioBuild2.load();
+});
+
+replMapBan1.on('change', (newValue, oldValue) => {
+  eMapBan1Div.style = `background-image: url('${newValue["imageUrl"]}'); background-repeat: no-repeat; background-position: center; background-size: cover;`;
+  eMapBan1Div.getElementsByClassName('mapText')[0].innerText = newValue["name"];
+  eMapBan1Div.getElementsByClassName('mapSubtext')[0].innerText = newValue["text"];
+});
+
+replMapBan2.on('change', (newValue, oldValue) => {
+  eMapBan2Div.style = `background-image: url('${newValue["imageUrl"]}'); background-repeat: no-repeat; background-position: center; background-size: cover;`;
+  eMapBan2Div.getElementsByClassName('mapText')[0].innerText = newValue["name"];
+  eMapBan2Div.getElementsByClassName('mapSubtext')[0].innerText = newValue["text"];
+});
+
+replMapBan3.on('change', (newValue, oldValue) => {
+  eMapBan3Div.getElementsByClassName('mapVideo')[0].getElementsByClassName('mapVideoSrc')[0].src = newValue["videoUrl"];
+  console.log(newValue["videoUrl"])
+  eMapBan3Div.getElementsByClassName('mapVideo')[0].load();
+  if (newValue["active"]) {
+    eMapBan3Div.getElementsByClassName('mapVideo')[0].play();
+    activeMap = eMapBan3Div;
+  }
+  eMapBan3Div.getElementsByClassName('mapText')[0].innerText = newValue["name"];
+  eMapBan3Div.getElementsByClassName('mapSubtext')[0].innerText = newValue["text"];
+});
+
+replMapBan4.on('change', (newValue, oldValue) => {
+  eMapBan4Div.getElementsByClassName('mapVideo')[0].getElementsByClassName('mapVideoSrc')[0].src = newValue["videoUrl"];
+  console.log(newValue["videoUrl"])
+  eMapBan4Div.getElementsByClassName('mapVideo')[0].load();
+  if (newValue["active"]) {
+    eMapBan4Div.getElementsByClassName('mapVideo')[0].play();
+    activeMap = eMapBan4Div;
+  }
+  eMapBan4Div.getElementsByClassName('mapText')[0].innerText = newValue["name"];
+  eMapBan4Div.getElementsByClassName('mapSubtext')[0].innerText = newValue["text"];
+});
+
+replMapBan5.on('change', (newValue, oldValue) => {
+  eMapBan5Div.style = `background-image: url('${newValue["imageUrl"]}'); background-repeat: no-repeat; background-position: center; background-size: cover;`;
+  eMapBan5Div.getElementsByClassName('mapText')[0].innerText = newValue["name"];
+  eMapBan5Div.getElementsByClassName('mapSubtext')[0].innerText = newValue["text"];
+});
+
+replMapBan6.on('change', (newValue, oldValue) => {
+  eMapBan6Div.style = `background-image: url('${newValue["imageUrl"]}'); background-repeat: no-repeat; background-position: center; background-size: cover;`;
+  eMapBan6Div.getElementsByClassName('mapText')[0].innerText = newValue["name"];
+  eMapBan6Div.getElementsByClassName('mapSubtext')[0].innerText = newValue["text"];
+});
+
+replMapBan7.on('change', (newValue, oldValue) => {
+  eMapBan7Div.getElementsByClassName('mapVideo')[0].getElementsByClassName('mapVideoSrc')[0].src = newValue["videoUrl"];
+  console.log(newValue["videoUrl"])
+  eMapBan7Div.getElementsByClassName('mapVideo')[0].load();
+  if (newValue["active"]) {
+    eMapBan7Div.getElementsByClassName('mapVideo')[0].play();
+    activeMap = eMapBan7Div;
+  }
+  eMapBan7Div.getElementsByClassName('mapText')[0].innerText = newValue["name"];
+  eMapBan7Div.getElementsByClassName('mapSubtext')[0].innerText = newValue["text"];
+});
+
+nodecg.listenFor('inflate', () => {
+  expandMapBan(activeMap);
+});
+
+nodecg.listenFor('deflate', () => {
+  deflateMapBan(eMapBan3Div);
+  deflateMapBan(eMapBan4Div);
+  deflateMapBan(eMapBan7Div);
+});
+
+/* Functions */
+
+const expandMapBan = (mapBanDiv) => {
+  const topOffset = mapBanDiv.getBoundingClientRect().top;
+  const leftOffset = mapBanDiv.getBoundingClientRect().left;
+  const width = mapBanDiv.getBoundingClientRect().width;
+  const height = mapBanDiv.getBoundingClientRect().height;
+
+  const computedStyle = window.getComputedStyle(mapBanDiv);
+  const divLMargin = parseInt(computedStyle.marginLeft);
+  const divRMargin = parseInt(computedStyle.marginRight);
+
+  mapBanDiv.dataset.topOffset = topOffset;
+  mapBanDiv.dataset.leftOffset = leftOffset - divLMargin;
+  mapBanDiv.dataset.height = height;
+  mapBanDiv.dataset.width = width;
+
+  mapBanDiv.style.top = `${topOffset}px`;
+  mapBanDiv.style.left = `${leftOffset - divLMargin}px`;
+
+  eAudioBuild2.play();
+
+  setTimeout(() => {
+    mapBanDiv.getElementsByClassName('mapVideoContainer')[0].style.filter = 'brightness(1)'
+    mapBanDiv.getElementsByClassName('mapText')[0].style.fontSize = '1.5em';
+    mapBanDiv.getElementsByClassName('mapSubtext')[0].style.fontSize = '1em';
+    mapBanDiv.style.position = 'fixed';
+    mapBanDiv.style.top = `0px`;
+    mapBanDiv.style.left = -divLMargin +'px';
+    mapBanDiv.style.height = '100vh';
+    mapBanDiv.style.width = '100vw';
+    mapBanDiv.style.borderLeftWidth = '0px';
+    mapBanDiv.style.borderBottomWidth = '0px';
+    mapBanDiv.style.borderRadius = '0px';
+    mapBanDiv.style.zIndex = '1000';
+
+    const expansionNode = document.createElement('div');
+    expansionNode.style.width = `${width + divLMargin + divRMargin}px`;
+    expansionNode.classList.add('expansionCompensation');
+    mapBanDiv.parentElement.insertBefore(expansionNode, mapBanDiv);
+
+    eAudioBuild1.pause();
+    eAudioBuild1.currentTime = 0;
+  }, 100)
+}
+
+const deflateMapBan = (mapBanDiv) => {
+  mapBanDiv.style.top = mapBanDiv.dataset.topOffset + 'px';
+  mapBanDiv.style.left = mapBanDiv.dataset.leftOffset + 'px';
+  mapBanDiv.style.height = mapBanDiv.dataset.height + 'px';
+  mapBanDiv.style.width = mapBanDiv.dataset.width + 'px';
+  mapBanDiv.style.borderLeftWidth = '';
+  mapBanDiv.style.borderBottomWidth = '';
+  mapBanDiv.style.borderRadius = '';
+
+  mapBanDiv.getElementsByClassName('mapVideoContainer')[0].style.filter = ''
+
+  mapBanDiv.getElementsByClassName('mapText')[0].style.fontSize = '1em';
+  mapBanDiv.getElementsByClassName('mapSubtext')[0].style.fontSize = '0.65em';
+
+  eAudioBuild1.play();
+  eAudioBuild2.pause();
+  eAudioBuild2.currentTime = 0;
+
+  setTimeout(() => {
+    mapBanDiv.style.position = 'unset';    
+    mapBanDiv.style.zIndex = '';
+
+    for (let e of document.getElementsByClassName('expansionCompensation')) {
+      e.remove();
+    }
+  }, 1500)
+}
